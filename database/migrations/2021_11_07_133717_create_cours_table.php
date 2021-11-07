5b4b869c384aaf6cc055fad3356496cbabb1a7a9
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-11-07 15:09:25 +0100

    one

---
+++ database/migrations/2021_11_07_133717_create_cours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cours', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug', 191)->unique();
+            $table->text('desc');
+            $table->string('created_by');
+            $table->string('url');
+            $table->string('img');
+            $table->bigInteger('category_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cours');
+    }
+}
