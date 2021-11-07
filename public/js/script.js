3b0d8a0348587357559e70c1a8b977f98f7e9b67
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-11-07 18:29:34 +0100

    updated

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+//function for showing th nav menu wheem click the nav button
+function showNav() {
+        var nav = document.querySelector('.navList');
+        nav.style.display = 'block';
+}
+ 
+//function for hidding the nav menu wheen clicking the close button
+function closMenu() {
+    var nav = document.querySelector('.navList');
+    nav.style.display = 'none';
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,73 +7,61 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>{{ config('app.name', 'FreeCours') }}</title>
+    <link rel="stylesheet"  href="{{ asset('css/styling.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('/js/script.js') }}">  </script>
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+    <div class="navbar_top">
+        <div class="navLogo">
+            <a href="/"> <img src="{{ asset('/img/logo.png') }}" alt="freecours logo"> </a>
+        </div>
+        
+        <div  class="navList">
+                <div onclick="closMenu()" class="close ">
+                    <i class="fa fa-times" aria-hidden="true"></i>
                 </div>
-            </div>
-        </nav>
+            <ul> 
+                <li><a href="">Home</a></li>
+                <li><a href="">Categories</a></li>
+                <li> <a href=""> Courses </a> </li>
+                    @if (Auth::check())
+                        <li class="profile"> <a href="">Profile</a></li>
+                        @else
+                            <li class="login">
+                                <a  href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+
+                            <li class="register">
+                                <a href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                    @endif
+            </ul>
+        </div>
+
+        <div class="searshing">
+            <form action="#" method="get">
+                <div> <input type="text" name="search" placeholder="Search"> </div>
+                <div> <button type="submit"> <i class="fa fa-search" aria-hidden="true"></i> </button> </div>
+            </form>
+        </div>
+
+        <div onclick="showNav()" class="birgerNav" >
+            <i class="fa fa-navicon" aria-hidden="true"></i>
+        </div>
+
+    </div>
 
         <main class="py-4">
             @yield('content')
