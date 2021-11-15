d3820da10f806012cef64eb6a9fe3a327002c3ed
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-11-15 10:11:35 +0100

    update

--- resources/views/cours/search.blade.php
+++ resources/views/cours/search.blade.php
@@ -13,26 +13,23 @@
                 <span></span>
             </div>
             <div class="____text">
-                <h3 class="m-5 text-center">  Courses Search Result  </h3>
+                <h2 class="text-center">  Courses Search Result  </h2>
             </div>
-
         </div>
-  
         <div class="all_courses_showing ">
             <div class="group_for_all">
-
-            @foreach ($courses as $cours)
-                <div class="cart_cours">
-                    <a href="{{ route('cours.details',$cours->slug) }}">
-                        <div class="card_img"> <img src="{{ asset('/storage/'.$cours->img) }}" alt="">  </div>
-                        <div class="card_cours_body">
-                            <div class="title_cours"><b> {{ Str::limit($cours->title, 50, '...') }} </b></div>
-                            <div class="desc_cours">{{ Str::limit(htmlspecialchars(trim(strip_tags($cours->desc))), 30, '...') }}</div>
-                            <div> <a href="{{ route('cours.details',$cours->slug) }}"><button>Show Details</button></a> </div>
-                        </div>  
-                    </a>
-                </div>
-            @endforeach
+                @foreach ($courses as $cours)
+                    <div class="cart_cours">
+                        <a href="{{ route('cours.details',$cours->slug) }}">
+                            <div class="card_img"> <img src="{{ asset('/storage/'.$cours->img) }}" alt="">  </div>
+                            <div class="card_cours_body">
+                                <div class="title_cours"><b> {{ Str::limit($cours->title, 50, '...') }} </b></div>
+                                <div class="desc_cours">{{ Str::limit(htmlspecialchars(trim(strip_tags($cours->desc))), 30, '...') }}</div>
+                                <div> <a href="{{ route('cours.details',$cours->slug) }}"><button>Show Details</button></a> </div>
+                            </div>  
+                        </a>
+                    </div>
+                @endforeach
                 @if($courses->count() == 0)
                     <div class="text-center mt-5 mb-5">
                         <p style="font-size : 20px;">Sorry! There No Courses Right Now</p>
